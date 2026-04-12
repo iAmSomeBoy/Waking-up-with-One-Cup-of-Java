e7271686e06a643c481558b55b65164d10a20acd
iAmSomeBoy <ashikur2401@gmail.com>
2026-04-13 01:50:08 +0600

    feat: add JPanel and JFrame examples for component organization and layout

---
+++ warm_up_with_java/java_swing/JPanel/JPanelExample.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+class jPanel{
+    jPanel(){
+        JFrame frame= new JFrame("Panel is for Partition");
+        JPanel panel= new JPanel();
+        panel.setBounds(40, 20, 200, 200);
+        JButton b= new JButton("panel Area");
+        b.setBounds(50 ,60,20 , 10);
+        panel.add(b);
+        frame.add(panel);
+        frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
+
+public class JPanelExample {
+    public static void main(String[] args){
+        new jPanel();
+    }
+}
