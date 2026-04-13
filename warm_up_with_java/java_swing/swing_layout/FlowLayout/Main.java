0eebed60264f7b0afc1169099a6c0ef77404b5aa
iAmSomeBoy <ashikur2401@gmail.com>
2026-04-13 14:51:20 +0600

    feat: implement FlowLayout example with button components in JFrame

---
+++ warm_up_with_java/java_swing/swing_layout/FlowLayout/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+class FlowLayoutClass{
+    JFrame frame;
+    FlowLayoutClass(){
+        frame= new JFrame("FlowLayout Example");
+
+        //create button components
+        JButton b1= new JButton("A");
+        JButton b2= new JButton("B");
+        JButton b3= new JButton("C");
+        JButton b4= new JButton("D");
+        JButton b5= new JButton("E");
+
+        //add components to the frame
+        frame.add(b1);
+        frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
+        frame.add(b5);
+
+        //set layout as 'FlowLayout.CENTER';
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        new FlowLayoutClass();
+    }
+}
