0f45ae4bcec459bb4b24f732a8f01aa7a5693dae
iAmSomeBoy <ashikur2401@gmail.com>
2026-04-15 18:29:54 +0600

    feat: add Examole1 class demonstrating exception handling in Java

---
+++ warm_up_with_java/Exception_handing_in_java/Examole1.java
@@ -0,0 +1,19 @@
+public class Examole1{
+    public static void main(String[] args){
+        int num1, num2;
+        try{
+            num1= 0; num2= 50/num1;
+            System.out.println(num2);
+            System.out.println("Last line of try block");
+
+        } catch(ArithmeticException e){
+            System.out.println("You should not divide a number by zero");
+
+        }
+        catch(Exception e){
+            System.out.println("Exception occured");
+        }
+
+        System.out.println("I'm out of try-catch block in java");
+    }
+}
