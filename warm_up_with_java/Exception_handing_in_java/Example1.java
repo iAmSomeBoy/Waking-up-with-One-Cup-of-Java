d71711230d9be9eda0bdcff19bc368f6b948bd84
iAmSomeBoy <ashikur2401@gmail.com>
2026-04-15 18:39:58 +0600

    feat: remove Examole1 class and add Example1 and Example2 classes demonstrating exception handling in Java

--- warm_up_with_java/Exception_handing_in_java/Example1.java
+++ warm_up_with_java/Exception_handing_in_java/Example1.java
@@ -1,4 +1,4 @@
-public class Examole1{
+public class Example1{
     public static void main(String[] args){
         int num1, num2;
         try{
---
+++ warm_up_with_java/Exception_handing_in_java/Example2.java
@@ -0,0 +1,22 @@
+public class Example2 {
+    public static void main(String[] args) {
+        try{
+            int[] a= new int[7];
+            a[4]= 30/0;
+            System.out.println("try block's statement");
+
+        }
+        catch(ArithmeticException e){
+            System.out.println("Warning: ArithmeticException");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Warning: ArrayIndexOutofBoundException");
+        }
+        catch(Exception e){
+            System.out.println("Warning: Some other exception");
+        }
+
+        System.out.println("Out of try-catch block");
+        
+    }
+}
