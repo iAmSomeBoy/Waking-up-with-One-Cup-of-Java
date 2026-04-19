124bdc618d2da5ecac2702363338bf8c3e349fba
iAmSomeBoy <ashikur2401@gmail.com>
2026-04-19 21:35:27 +0600

    feat: add Vector example demonstrating element addition and iteration

---
+++ warm_up_with_java/collections/Vector/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Vector<String> v= new Vector<>();
+        v.add("item1");   //["item1"]
+        v.add("item2");     //["item1", "item2"]
+        v.add(0,"item3");   //["item1", "item2", "item3"]
+
+        Iterator<String> itr= v.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+    }
+}
